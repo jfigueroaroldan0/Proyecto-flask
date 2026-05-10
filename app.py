edcd883c647ac52b959976777a6d7a9b3ccf07ef
Jesús Figueroa Roldán <verdeblancoyazul@gmail.com>
2026-05-10 14:20:26 +0200

    Añadido ruta página principal con link al catalógo de motos

--- app.py
+++ app.py
@@ -16,4 +16,8 @@ def formato_precio(value):
     except (ValueError, TypeError):
         return value
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
 app.run("0.0.0.0",5000,debug=True)

a759c07aa9f8db2d4edb12d51783f7c7b889c1c1
Jesús Figueroa Roldán <verdeblancoyazul@gmail.com>
2026-05-10 13:40:49 +0200

    Añadido estructura app.py y pequeños cambios en HTML

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 import flask
+import os
+from flask import Flask, render_template, request, abort
+
 app = Flask(__name__)
 
+JSON_PATH = os.path.join(os.path.dirname(__file__), "data", "motos.json")
 
+with open(JSON_PATH, encoding="utf-8") as f:
+    MOTOS = json.load(f)
+
+@app.template_filter("formato_precio")
+def formato_precio(value):
+    try:
+        return f"{int(value):,} €".replace(",", ".")
+    except (ValueError, TypeError):
+        return value
 
 app.run("0.0.0.0",5000,debug=True)
